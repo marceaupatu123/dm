4411e26a9053282b77e74b365d6e3f564a8174fc
Maeva <ayko.pyt@gmail.com>
2026-09-15 17:15:45 +0200

    J'ai fini len.sh Fixes #2

--- len.sh
+++ len.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 # Implémentation de len.sh à faire 
+
+for mot in "$@"; do
+    len=0
+    while [ $mot -gt 0 ]; do
+        mot=$(expr $mot / 10)
+        len=$(expr $len + 1)
+    done
+    if [ $mot -eq 0 ]; then
+        echo "$len"
+    fi
+done
